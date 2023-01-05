eb2eb33dccb4b9bab030b19356142c184bc9b7cd
Dimitri <dimitri.rayer@pasquier.fr>
2023-01-05 11:32:37 +0100

    clean : remove types folder

--- types/useSimulation.d.ts
+++
@@ -1,7 +0,0 @@
-import { D3Link, D3Node, D3Options } from './types';
-import { Simulation } from "d3-force";
-import { Ref } from "vue";
-export declare function useSimulation(options: D3Options): {
-    simulation: Ref<Simulation<D3Node, undefined> | undefined>;
-    animate: (nodes: D3Node[], links: D3Link[]) => void;
-};
